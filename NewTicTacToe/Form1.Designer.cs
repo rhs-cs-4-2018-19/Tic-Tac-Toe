2602b6fc1abd1a1f8f2b4fdff293db19f2d80f21
Tyler Peaster <tyler.peaster@riversiderebels.net>
2019-04-25 09:33:04 -0500

    All of the code is inserted but the TicTacToe still won't place X or O.

--- NewTicTacToe/Form1.Designer.cs
+++ NewTicTacToe/Form1.Designer.cs
@@ -28,44 +28,281 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.txtTopLeft = new System.Windows.Forms.TextBox();
+            this.txtTopMiddle = new System.Windows.Forms.TextBox();
+            this.txtTopRight = new System.Windows.Forms.TextBox();
+            this.txtMiddleLeft = new System.Windows.Forms.TextBox();
+            this.txtMiddleMiddle = new System.Windows.Forms.TextBox();
+            this.txtMiddleRight = new System.Windows.Forms.TextBox();
+            this.txtBottomLeft = new System.Windows.Forms.TextBox();
+            this.txtBottomMiddle = new System.Windows.Forms.TextBox();
+            this.txtBottomRight = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnMark = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // txtMessage
             // 
-            this.button1.Location = new System.Drawing.Point(451, 138);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(67, 64);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.txtMessage.BackColor = System.Drawing.Color.White;
+            this.txtMessage.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txtMessage.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMessage.Location = new System.Drawing.Point(12, 12);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.ReadOnly = true;
+            this.txtMessage.Size = new System.Drawing.Size(249, 54);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.TabStop = false;
+            this.txtMessage.Text = "X\'s Turn";
+            this.txtMessage.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // textBox1
+            // textBox2
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 12);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(249, 20);
-            this.textBox1.TabIndex = 1;
+            this.textBox2.BackColor = System.Drawing.Color.Black;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox2.Location = new System.Drawing.Point(12, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(249, 13);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.Black;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox3.Location = new System.Drawing.Point(12, 225);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(249, 13);
+            this.textBox3.TabIndex = 3;
+            this.textBox3.TabStop = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.BackColor = System.Drawing.Color.Black;
+            this.textBox4.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox4.Location = new System.Drawing.Point(173, 72);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(22, 233);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TabStop = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.Black;
+            this.textBox5.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.textBox5.Location = new System.Drawing.Point(78, 72);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(22, 233);
+            this.textBox5.TabIndex = 5;
+            this.textBox5.TabStop = false;
+            // 
+            // txtTopLeft
+            // 
+            this.txtTopLeft.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txtTopLeft.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTopLeft.ForeColor = System.Drawing.Color.Blue;
+            this.txtTopLeft.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.txtTopLeft.Location = new System.Drawing.Point(12, 72);
+            this.txtTopLeft.Multiline = true;
+            this.txtTopLeft.Name = "txtTopLeft";
+            this.txtTopLeft.ReadOnly = true;
+            this.txtTopLeft.Size = new System.Drawing.Size(60, 61);
+            this.txtTopLeft.TabIndex = 6;
+            this.txtTopLeft.TabStop = false;
+            this.txtTopLeft.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTopMiddle
+            // 
+            this.txtTopMiddle.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txtTopMiddle.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTopMiddle.ForeColor = System.Drawing.Color.Blue;
+            this.txtTopMiddle.Location = new System.Drawing.Point(106, 72);
+            this.txtTopMiddle.Multiline = true;
+            this.txtTopMiddle.Name = "txtTopMiddle";
+            this.txtTopMiddle.ReadOnly = true;
+            this.txtTopMiddle.Size = new System.Drawing.Size(60, 61);
+            this.txtTopMiddle.TabIndex = 7;
+            this.txtTopMiddle.TabStop = false;
+            this.txtTopMiddle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtTopRight
+            // 
+            this.txtTopRight.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txtTopRight.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTopRight.ForeColor = System.Drawing.Color.Blue;
+            this.txtTopRight.Location = new System.Drawing.Point(201, 72);
+            this.txtTopRight.Multiline = true;
+            this.txtTopRight.Name = "txtTopRight";
+            this.txtTopRight.ReadOnly = true;
+            this.txtTopRight.Size = new System.Drawing.Size(60, 61);
+            this.txtTopRight.TabIndex = 8;
+            this.txtTopRight.TabStop = false;
+            this.txtTopRight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtMiddleLeft
+            // 
+            this.txtMiddleLeft.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txtMiddleLeft.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiddleLeft.ForeColor = System.Drawing.Color.Blue;
+            this.txtMiddleLeft.Location = new System.Drawing.Point(12, 158);
+            this.txtMiddleLeft.Multiline = true;
+            this.txtMiddleLeft.Name = "txtMiddleLeft";
+            this.txtMiddleLeft.ReadOnly = true;
+            this.txtMiddleLeft.Size = new System.Drawing.Size(60, 61);
+            this.txtMiddleLeft.TabIndex = 9;
+            this.txtMiddleLeft.TabStop = false;
+            this.txtMiddleLeft.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtMiddleMiddle
+            // 
+            this.txtMiddleMiddle.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txtMiddleMiddle.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiddleMiddle.ForeColor = System.Drawing.Color.Blue;
+            this.txtMiddleMiddle.Location = new System.Drawing.Point(106, 158);
+            this.txtMiddleMiddle.Multiline = true;
+            this.txtMiddleMiddle.Name = "txtMiddleMiddle";
+            this.txtMiddleMiddle.ReadOnly = true;
+            this.txtMiddleMiddle.Size = new System.Drawing.Size(60, 61);
+            this.txtMiddleMiddle.TabIndex = 10;
+            this.txtMiddleMiddle.TabStop = false;
+            this.txtMiddleMiddle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtMiddleRight
+            // 
+            this.txtMiddleRight.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txtMiddleRight.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMiddleRight.ForeColor = System.Drawing.Color.Blue;
+            this.txtMiddleRight.Location = new System.Drawing.Point(201, 158);
+            this.txtMiddleRight.Multiline = true;
+            this.txtMiddleRight.Name = "txtMiddleRight";
+            this.txtMiddleRight.ReadOnly = true;
+            this.txtMiddleRight.Size = new System.Drawing.Size(60, 61);
+            this.txtMiddleRight.TabIndex = 11;
+            this.txtMiddleRight.TabStop = false;
+            this.txtMiddleRight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtBottomLeft
+            // 
+            this.txtBottomLeft.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txtBottomLeft.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBottomLeft.ForeColor = System.Drawing.Color.Blue;
+            this.txtBottomLeft.Location = new System.Drawing.Point(12, 244);
+            this.txtBottomLeft.Multiline = true;
+            this.txtBottomLeft.Name = "txtBottomLeft";
+            this.txtBottomLeft.ReadOnly = true;
+            this.txtBottomLeft.Size = new System.Drawing.Size(60, 61);
+            this.txtBottomLeft.TabIndex = 12;
+            this.txtBottomLeft.TabStop = false;
+            this.txtBottomLeft.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtBottomMiddle
+            // 
+            this.txtBottomMiddle.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txtBottomMiddle.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBottomMiddle.ForeColor = System.Drawing.Color.Blue;
+            this.txtBottomMiddle.Location = new System.Drawing.Point(107, 244);
+            this.txtBottomMiddle.Multiline = true;
+            this.txtBottomMiddle.Name = "txtBottomMiddle";
+            this.txtBottomMiddle.ReadOnly = true;
+            this.txtBottomMiddle.Size = new System.Drawing.Size(60, 61);
+            this.txtBottomMiddle.TabIndex = 13;
+            this.txtBottomMiddle.TabStop = false;
+            this.txtBottomMiddle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtBottomRight
+            // 
+            this.txtBottomRight.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.txtBottomRight.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBottomRight.ForeColor = System.Drawing.Color.Blue;
+            this.txtBottomRight.Location = new System.Drawing.Point(201, 244);
+            this.txtBottomRight.Multiline = true;
+            this.txtBottomRight.Name = "txtBottomRight";
+            this.txtBottomRight.ReadOnly = true;
+            this.txtBottomRight.Size = new System.Drawing.Size(60, 61);
+            this.txtBottomRight.TabIndex = 14;
+            this.txtBottomRight.TabStop = false;
+            this.txtBottomRight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(78, 315);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(117, 23);
+            this.btnReset.TabIndex = 15;
+            this.btnReset.Text = "Reset Game";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnMark
+            // 
+            this.btnMark.Location = new System.Drawing.Point(78, 344);
+            this.btnMark.Name = "btnMark";
+            this.btnMark.Size = new System.Drawing.Size(117, 23);
+            this.btnMark.TabIndex = 16;
+            this.btnMark.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 350);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(274, 340);
+            this.Controls.Add(this.btnMark);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtBottomRight);
+            this.Controls.Add(this.txtBottomMiddle);
+            this.Controls.Add(this.txtBottomLeft);
+            this.Controls.Add(this.txtMiddleRight);
+            this.Controls.Add(this.txtMiddleMiddle);
+            this.Controls.Add(this.txtMiddleLeft);
+            this.Controls.Add(this.txtTopRight);
+            this.Controls.Add(this.txtTopMiddle);
+            this.Controls.Add(this.txtTopLeft);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtMessage);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tic-Tac-Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox txtTopLeft;
+        private System.Windows.Forms.TextBox txtTopMiddle;
+        private System.Windows.Forms.TextBox txtTopRight;
+        private System.Windows.Forms.TextBox txtMiddleLeft;
+        private System.Windows.Forms.TextBox txtMiddleMiddle;
+        private System.Windows.Forms.TextBox txtMiddleRight;
+        private System.Windows.Forms.TextBox txtBottomLeft;
+        private System.Windows.Forms.TextBox txtBottomMiddle;
+        private System.Windows.Forms.TextBox txtBottomRight;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnMark;
     }
 }
 
--- NewTicTacToe/Form1.cs
+++ NewTicTacToe/Form1.cs
@@ -1,20 +1,110 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace NewTicTacToe
+
 {
     public partial class Form1 : Form
     {
+
+        bool xTurn; // if true, it's X's turn
+        TextBox squareClicked; // tells which text box is clicked
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // make it X's turn
+            xTurn = true;
+        }
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            // Clear board
+            txtTopLeft.Text = "";
+            txtTopMiddle.Text = "";
+            txtTopRight.Text = "";
+            txtMiddleLeft.Text = "";
+            txtMiddleMiddle.Text = "";
+            txtMiddleRight.Text = "";
+            txtBottomLeft.Text = "";
+            txtBottomMiddle.Text = "";
+            txtBottomRight.Text = "";
+            // Make it X's turn
+            xTurn = true;
+            txtMessage.Text = "X's Turn";
+        }
+
+        private void btnMark_Click(object sender, EventArgs e)
+        {
+            // Place mark in SquareClicked (if none there already)
+            if (squareClicked.Text == "")
+            {
+                if (xTurn)
+                {
+                    squareClicked.Text = "X";
+                    xTurn = false;
+                    txtMessage.Text = "O's Turn";
+                }
+                else
+                {
+                    squareClicked.Text = "O";
+                    xTurn = true;
+                    txtMessage.Text = "X's Turn";
+                }
+            }
+        }
+
+        private void txtTopLeft_Click(object sender, EventArgs e)
+        {
+            squareClicked = txtTopLeft;
+            btnMark.PerformClick();
+        }
+        private void txtTopMiddle_Click(object sender, EventArgs e)
+        {
+            squareClicked = txtTopMiddle;
+            btnMark.PerformClick();
+        }
+        private void txtTopRight_Click(object sender, EventArgs e)
+        {
+            squareClicked = txtTopRight;
+            btnMark.PerformClick();
+        }
+        private void txtMiddleLeft_Click(object sender, EventArgs e)
+        {
+            squareClicked = txtMiddleLeft;
+            btnMark.PerformClick();
+        }
+        private void txtMiddleMiddle_Click(object sender, EventArgs
+        e)
+        {
+            squareClicked = txtMiddleMiddle;
+            btnMark.PerformClick();
+        }
+        private void txtMiddleRight_Click(object sender, EventArgs e)
+        {
+            squareClicked = txtMiddleRight;
+            btnMark.PerformClick();
+        }
+        private void txtBottomLeft_Click(object sender, EventArgs e)
+        {
+            squareClicked = txtBottomLeft;
+            btnMark.PerformClick();
+        }
+        private void txtBottomMiddle_Click(object sender, EventArgs
+        e)
+        {
+            squareClicked = txtBottomMiddle;
+            btnMark.PerformClick();
+        }
+        private void txtBottomRight_Click(object sender, EventArgs e)
+        {
+            squareClicked = txtBottomRight;
+            btnMark.PerformClick();
+        }
     }
+
 }
